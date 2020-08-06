8f4079c74875098dafe9465e94dd6cf4ba1682cc
Austin Cummings <aac729@live.com>
2020-08-06 21:43:58 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,10 @@
 # code here!
+class School
+ def initialize(school)
+   @school= school 
+ end 
+end 
+
+school = School.new("Bayside High School")
+school.add_student("Zach Morris")
+school.roster
